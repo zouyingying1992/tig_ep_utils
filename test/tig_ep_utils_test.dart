c5c0dc01c42bb2125a5f6ad901f3685b9bc6b689
Nian <i19850511@gmail.com>
2021-05-13 15:22:23 +0800

    fix

--- test/tig_ep_utils_test.dart
+++ test/tig_ep_utils_test.dart
@@ -3,10 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tig_ep_utils/tig_ep_utils.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('Tests not implemented', () {
+    expect(1, 1);
   });
 }
