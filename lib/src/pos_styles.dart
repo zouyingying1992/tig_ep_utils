5500f5381ca2dff5f1942c0385aa35b4b3bdbd1f
Nian <i19850511@gmail.com>
2021-05-14 09:25:18 +0800

    version

--- lib/src/pos_styles.dart
+++ lib/src/pos_styles.dart
@@ -18,21 +18,8 @@ class PosStyles {
     this.align = PosAlign.left,
     this.height = PosTextSize.size1,
     this.width = PosTextSize.size1,
-    this.fontType,
+    this.fontType = PosFontType.fontA,
     this.codeTable,
-  });
-
-  // Init all fields with default values
-  const PosStyles.defaults({
-    this.bold: false,
-    this.reverse: false,
-    this.underline: false,
-    this.turn90: false,
-    this.align: PosAlign.left,
-    this.height: PosTextSize.size1,
-    this.width: PosTextSize.size1,
-    this.fontType: PosFontType.fontA,
-    this.codeTable: "CP437",
   });
 
   final bool bold;
@@ -43,29 +30,5 @@ class PosStyles {
   final PosTextSize height;
   final PosTextSize width;
   final PosFontType fontType;
-  final String codeTable;
-
-  PosStyles copyWith({
-    bool bold,
-    bool reverse,
-    bool underline,
-    bool turn90,
-    PosAlign align,
-    PosTextSize height,
-    PosTextSize width,
-    PosFontType fontType,
-    String codeTable,
-  }) {
-    return PosStyles(
-      bold: bold ?? this.bold,
-      reverse: reverse ?? this.reverse,
-      underline: underline ?? this.underline,
-      turn90: turn90 ?? this.turn90,
-      align: align ?? this.align,
-      height: height ?? this.height,
-      width: width ?? this.width,
-      fontType: fontType ?? this.fontType,
-      codeTable: codeTable ?? this.codeTable,
-    );
-  }
+  final PosCodeTable codeTable;
 }
--- lib/tig_ep_utils.dart
+++ lib/tig_ep_utils.dart
@@ -1,9 +1,7 @@
 library tig_ep_utils;
 
 export './src/barcode.dart';
-export './src/capability_profile.dart';
 export './src/enums.dart';
 export './src/pos_column.dart';
 export './src/pos_styles.dart';
-export './src/qrcode.dart';
-export './src/generator.dart';
+export './src/ticket.dart';
